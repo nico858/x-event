b7a72d0eae5fdd86f85ee9a49483ef3710050166
elkoreano <criistiian-95@hotmail.com>
2023-10-13 11:57:16 -0500

    adding event router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,8 @@
 import express from 'express';
 
+// routes for new project
+import eventRouter from './events.router.js';
+
 import userRouter from './user.router.js';
 // import addressRouter from './address.router.js';
 // import orderDateRouter from './orderDate.router.js';
@@ -15,6 +18,7 @@ export default function routerApi(app) {
   app.use(express.json());
   
   app.use('/', router);
+  router.use('/eventCreation', eventRouter);
 
   router.use('/user', userRouter);
 //   router.use('/address', addressRouter);
