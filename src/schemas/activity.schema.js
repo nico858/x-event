240895b0e679a4e833ff3260b9c35de5800410a2
nico858 <nicolasurbanos10@gmail.com>
2023-11-02 13:15:53 -0500

    Setting description to 500 characters

--- src/schemas/activity.schema.js
+++ src/schemas/activity.schema.js
@@ -3,7 +3,7 @@ import Joi from 'joi';
 const id = Joi.number().integer();
 const eventId = Joi.number().integer();
 const creatorId = Joi.number().integer();
-const description = Joi.string().min(5).max(40);
+const description = Joi.string().min(5).max(500);
 const cost = Joi.number().precision(2);
 const state = Joi.string();
 const active = Joi.boolean();
--- src/schemas/event.schema.js
+++ src/schemas/event.schema.js
@@ -3,7 +3,7 @@ import Joi from 'joi';
 const id = Joi.number().integer();
 const creator = Joi.number().integer();
 const name = Joi.string().min(5).max(40);
-const description = Joi.string().min(5).max(40);
+const description = Joi.string().min(5).max(500);
 const type = Joi.string();
 const photo = Joi.binary();
 const cost = Joi.number().precision(2);
